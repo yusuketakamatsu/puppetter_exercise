c95d1a21e4918b314504905f43a7b0e19361c902
yusuketakamatsu <sleepingforest03@gmail.com>
2020-02-18 00:55:24 +0900

    parallel request.

--- src/get.js
+++ src/get.js
@@ -4,14 +4,13 @@ const fs = require('fs');
 const getPage = async (browser, target, domain) => {
   let protocol = "https://";
   let page = await browser.newPage();
-
-  console.log("url: " + protocol + domain + target.path)
   const response = await page.goto(protocol + domain + target.path);
   // await page.screenshot({
   //     path: `puppeteer/img/${target.name}.png`,
   //     fullpage: true
   // });
-  console.log("status_code: " + response._status)
+  console.log("url: " + protocol + domain + target.path);
+  console.log("status_code: " + response._status);
   console.log("end.");
   await page.close();
 }
@@ -25,8 +24,8 @@ const getPage = async (browser, target, domain) => {
   });
   info = JSON.parse(fs.readFileSync(`puppeteer/${process.argv[2]}.json`));
   domain = info.domain;
-  for (const target of info.targets) {
-      await getPage(browser, target, domain);
-  }
+  await Promise.all(
+    info.targets.map(target => getPage(browser, target, domain))
+  );
   browser.close();
 })()
